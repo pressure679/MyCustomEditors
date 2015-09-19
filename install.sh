5f3655fbb875e3123324dae36ea7cd9a6cbfbdff
Vittus Mikiassen <pressure679@users.noreply.github.com>
2015-09-20 00:26:32 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt-get install vim emacs24 git mercurial irssi build-essential make cmake automake w3m w3m-img libpcap0.8-dev
+mkdir $HOME/.vim
+mkdir $HOME/.vim/
+mkdir $HOME/.emacs.d
+cd /home/$USER/.vim
+git clone https://github.com/VundleVim/Vundle.vim
+cd ..
