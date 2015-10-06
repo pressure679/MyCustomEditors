b59d9ab0a8b9a85c927e2d1cf580c0b74b466994
Vittus Mikiassen <pressure679@users.noreply.github.com>
2015-10-06 06:20:01 +0200

    Create install-vim-go.sh

---
+++ install-vim-go.sh
@@ -0,0 +1,32 @@
+echo "filetype off
+set rtp +=~/.vim/bundle/Vundle.vim
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'flazz/vim-colorschemes'
+Plugin 'fatih/vim-go'
+Plugin 'nsf/gocode'
+Plugin 'majutsushi/tagbar'
+Plugin 'Valloric/YouCompleteMe'
+Plugin 'SirVer/ultisnips'
+call vundle#end()
+let g:go_highlight_operators = 1
+let g:go_highlight_functions = 1
+let g:go_highlight_methods = 1
+let g:go_highlight_structs = 1
+let g:go_highlight_build_constraints = 1
+let g:go_fmt_autosave = 0
+
+let g:UltiSnipsExpandTrigger=\"<C-k>\"
+let g:UltiSnipsJumpForwardTrigger=\"<C-l>\"
+let g:UltiSnipsJumpBackwardTrigger=\"<C-j>\"
+filetype plugin on"
+
+cd .vim
+mkdir bundle
+cd bundle
+git clone https://github.com/VundleVim/Vundle.vim
+git clone https://github.com/fatih/vim-go
+git clone https://github.com/Valloric/YouCompleteMe
+git clone https://github.com/majutsushi/tagbar
+git clone https://github.com/SirVer/ultisnips
+git clone https://github.com/garyburd/go-explorer
+nimak$ 
