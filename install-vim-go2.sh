e5d6d0dcd6a8ec5737087c1c00532fa4b67c329f
Vittus Mikiassen <pressure679@users.noreply.github.com>
2015-10-08 16:44:57 +0200

    Update install-vim-go2.sh

--- install-vim-go2.sh
+++ install-vim-go2.sh
@@ -1,14 +1,3 @@
-cd $GOPATH/bin
-mv errcheck $GOROOT/bin
-mv gocode $GOROOT/bin
-mv goimports $GOROOT/bin
-mv gometalinter $GOROOT/bin
-mv gotags $GOROOT/bin
-mv godef $GOROOT/bin
-mv gofm $GOROOT/bin
-mv golint $GOROOT/bin
-mv gorename $GOROOT/bin
-mv oracle $GOROOT/bin
 cd $HOME/.vim/bundle/YouCompleteMe
 sudo apt-get install python-dev cmake build-essential ctags
 python install.py --gocode-completer
