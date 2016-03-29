8465ee3850fde665a72d8d997a27ff482a7e8107
Vittus Mikiassen <pressure679@users.noreply.github.com>
2016-03-30 01:57:52 +0200

    Update install-fedora.sh

--- install-fedora.sh
+++ install-fedora.sh
@@ -1,6 +1,6 @@
 sudo dnf update
 sudo dnf upgrade
-sudo dnf install emacs git mercurial irssi build-essential make cmake automake w3m python-dev ctags tlp google-chrome-stable vlc audacious htop openssh-server python-pip youtube-dl terminator samba system-config-samba xclip golang
+sudo dnf install emacs git mercurial irssi build-essential make cmake automake w3m python-dev ctags tlp google-chrome-stable vlc audacious htop openssh-server python-pip youtube-dl terminator samba system-config-samba xclip golang taglib-devel
 
 mkdir ~/.emacs.d
 wget https://raw.githubusercontent.com/pressure679/UbuntuEtcSetup/master/.emacs
@@ -25,6 +25,13 @@ cd ~/Downloads
 wget https://justgetflux.com/linux/xflux64.tgz
 sudo tar -xvzf xflux64.tgz -O /usr/bin
 sudo chmod +x /usr/bin/xflux
+cd ~/go
+mkdir id3go
+cd id3go
+wget https://raw.githubusercontent.com/pressure679/MP3ID3Tagger/master/sample.go
 sudo wget https://raw.githubusercontent.com/pressure679/UbuntuEtcSetup/master/environment -O /etc
+go get github.com/dustin/go-wikiparse
+go get github.com/pressure679/dijkstra
+go get github.com/wtolson/go-taglib
 
 echo 0 | sudo tee /sys/class/graphics/fbcon/cursor_blink
