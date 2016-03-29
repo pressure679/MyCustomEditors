8ea3b1dca8be63588b1a41b184d2f0ebc4fc55e0
Vittus Mikiassen <pressure679@users.noreply.github.com>
2016-03-30 01:27:45 +0200

    Create install-vim.sh

---
+++ install-vim.sh
@@ -0,0 +1,9 @@
+mkdir ~/.vim
+mkdir ~/.vim/bundle
+cd ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim
+git clone https://github.com/fatih/vim-go
+git clone https://github.com/Valloric/YouCompleteMe
+git clone https://github.com/majutsushi/tagbar
+git clone https://github.com/SirVer/ultisnips
+git clone https://github.com/garyburd/go-explorer
