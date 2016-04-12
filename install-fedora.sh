40b4b4bd479236bb66d27673ea2af32309c7cdcf
Vittus Mikiassen <pressure679@users.noreply.github.com>
2016-04-13 01:05:22 +0200

    Update install-fedora.sh

--- install-fedora.sh
+++ install-fedora.sh
@@ -1,12 +1,4 @@
-sudo dnf install git mercurial irssi make cmake automake w3m ctags tlp lm-sensors vlc audacious spotify-client htop openssh-server python-pip youtube-dl mp3gain synaptic terminator samba system-config-samba gufw xclip libtagc0-dev golang-go.tools emacs ffmpeg
-
-# wget https://yt-dl.org/downloads/2016.03.27/youtube-dl
-# mkdir /usr/share/youtube-dl
-# mkdir /usr/share/youtube-dl/bin
-# sudo mv youtube-dl /usr/share/youtube-dl/bin/youtube-dl
-# sudo chown root /usr/share/youtube-dl/bin/youtube-dl
-# sudo chmod +x /usr/share/youtube-dl/bin/youtube-dl
-# sudo ln -s /usr/share/youtube-dl/bin/youtube-dl /usr/bin
+sudo dnf install git mercurial irssi make cmake automake w3m ctags tlp lm-sensors audacious htop openssh-server python-pip youtube-dl synaptic terminator samba system-config-samba xclip taglib-devel golang golang-godoc emacs
 
 sudo pip install livestreamer livestreamer-curses
 
@@ -17,34 +9,30 @@ sudo tar -C /usr/share/xflux -xf xflux64.tgz
 sudo chown root /usr/share/xflux/xflux
 sudo ln -s /usr/share/xflux/xflux /usr/bin
 
-wget https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz
-sudo tar -C /usr/share -xzf go1.5.3.linux-amd64.tar.gz
-sudo chown root /usr/share/go -R
-
 sudo rm /etc/environment
 wget https://raw.githubusercontent.com/pressure679/UbuntuEtcSetup/master/environment
 sudo mv environment /etc
 sudo chown root /etc/environment
 
 export GOPATH=$HOME/go
-export GOROOT=/usr/share/go
-sudo ln -s /usr/share/go/bin/* /usr/bin
+export GOROOT=/usr/lib/golang
 go get github.com/dustin/go-wikiparse
 go get github.com/alixaxel/pagerank
-go get github.com/smallsmallwolf/dijkstra
+go get github.com/pressure679/dijkstra
 go get github.com/wtolson/go-taglib
 go get github.com/sjwhitworth/golearn
+go get github.com/Masterminds/squirrel
 cd $GOPATH/src/github.com/sjwhitworth/golearn
 go get ./...
 
-cd $HOME/Downloads
-https://drive.google.com/file/d/0B7iDWdwgu9QAZTI2YXNZSUJ4Y00/Ambiance-Blackout-Flat-Colors-16-04-1-LTS-GTK-3-18-Theme.tar.gz
-sudo tar -C /usr/share/themes -xf *Ambiance*.tar.gz
-sudo chown root /usr/share/themes/Ambiance* -R
+# cd $HOME/Downloads
+# https://drive.google.com/file/d/0B7iDWdwgu9QAZTI2YXNZSUJ4Y00/Ambiance-Blackout-Flat-Colors-16-04-1-LTS-GTK-3-18-Theme.tar.gz
+# sudo tar -C /usr/share/themes -xf *Ambiance*.tar.gz
+# sudo chown root /usr/share/themes/Ambiance* -R
 
 sudo wget http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-258269.jpg
 sudo cp wallhaven*.jpg /usr/share/xfce4/backdrops
-sudo cp wallhaven*.jpg /usr/share/backgrounds/xfce
+sudo cp wallhaven*.jpg /usr/share/backgrounds/
 
 wget https://raw.githubusercontent.com/pressure679/UbuntuEtcSetup/master/lightdm-gtk-greeter.conf
 sudo chown root lightdm-gtk-greeter.conf
@@ -111,80 +99,79 @@ wget https://raw.githubusercontent.com/Denommus/emacs-config/master/erc-better-s
 cp erc-better-scroll.el erc-better-scroll/
 rm erc-better-scroll.el
 
-mkdir erc-hl-nicks
-wget https://melpa.org/packages/erc-hl-nicks-20160202.1150.el
-cp erc-hl-nicks-*.el erc-hl-nicks/erc-hl-nicks.el
-rm erc-hl-nicks-*.el
+# mkdir erc-hl-nicks
+# wget https://melpa.org/packages/erc-hl-nicks-20160202.1150.el
+# cp erc-hl-nicks-*.el erc-hl-nicks/erc-hl-nicks.el
+# rm erc-hl-nicks-*.el
 
-wget https://melpa.org/packages/evil-20160406.252.tar
-tar -xf evil*.tar
-mv evil-* evil
+# wget https://melpa.org/packages/evil-20160406.252.tar
+# tar -xf evil*.tar
+# mv evil-* evil
 
-mkdir evil-escape
-wget https://melpa.org/packages/evil-escape-20160313.1205.el
-cp evil-escape*.el evil-escape/evil-escape.el
-rm evil-escape*.el
+# mkdir evil-escape
+# wget https://melpa.org/packages/evil-escape-20160313.1205.el
+# cp evil-escape*.el evil-escape/evil-escape.el
+# rm evil-escape*.el
 
-mkdir flymake-go
-wget https://melpa.org/packages/flymake-go-20150714.33.el
-cp flymake-go*.el flymake-go/flymake-go.el
-rm flymake-go*.el
+# mkdir flymake-go
+# wget https://melpa.org/packages/flymake-go-20150714.33.el
+# cp flymake-go*.el flymake-go/flymake-go.el
+# rm flymake-go*.el
 
-mkdir flymake-cursor
-wget https://melpa.org/packages/flymake-cursor-20130822.332.el
-cp flymake-cursor*.el flymake-cursor/flymake-cursor.el
-rm flymake-cursor*.el
+# mkdir flymake-cursor
+# wget https://melpa.org/packages/flymake-cursor-20130822.332.el
+# cp flymake-cursor*.el flymake-cursor/flymake-cursor.el
+# rm flymake-cursor*.el
 
-mkdir go-autocomplete
-wget https://melpa.org/packages/go-autocomplete-20150903.1940.el
-cp go-autocomplete*.el go-autocomplete/go-autocomplete.el
-rm go-autocomplete*.el
+# mkdir go-autocomplete
+# wget https://melpa.org/packages/go-autocomplete-20150903.1940.el
+# cp go-autocomplete*.el go-autocomplete/go-autocomplete.el
+# rm go-autocomplete*.el
 
-wget https://melpa.org/packages/go-mode-20160404.2.tar
-tar -xf go-mode*.tar
-mv go-mode-*.tar $HOME/Downloads
-mv go-mode-* go-mode
+# wget https://melpa.org/packages/go-mode-20160404.2.tar
+# tar -xf go-mode*.tar
+# mv go-mode-*.tar $HOME/Downloads
+# mv go-mode-* go-mode
 
-mkdir goto-chg
-wget https://melpa.org/packages/goto-chg-20131228.659.el
-cp goto-chg*.el goto-chg/goto-chg.el
-rm goto-chg*.el
+# mkdir goto-chg
+# wget https://melpa.org/packages/goto-chg-20131228.659.el
+# cp goto-chg*.el goto-chg/goto-chg.el
+# rm goto-chg*.el
 
-mkdir hlinum
-wget https://melpa.org/packages/hlinum-20150621.1933.el
-cp hlinum*.el hlinum/hlinum.el
-rm hlinum*.el
+# mkdir hlinum
+# wget https://melpa.org/packages/hlinum-20150621.1933.el
+# cp hlinum*.el hlinum/hlinum.el
+# rm hlinum*.el
 
-mkdir popup
-wget https://melpa.org/packages/popup-20151222.1339.el
-cp popup*.el popup/popup.el
-rm popup*.el
+# mkdir popup
+# wget https://melpa.org/packages/popup-20151222.1339.el
+# cp popup*.el popup/popup.el
+# rm popup*.el
 
-wget https://melpa.org/packages/yasnippet-20160408.1558.tar
-tar -xf yasnippet*.tar
-mv yasnippet-* yasnippet
+# wget https://melpa.org/packages/yasnippet-20160408.1558.tar
+# tar -xf yasnippet*.tar
+# mv yasnippet-* yasnippet
 
-mkdir undo-tree
-wget https://melpa.org/packages/undo-tree-20140509.522.el
-cp undo-tree-*.el undo-tree/undo-tree.el
-rm undo-tree-*.el
+# mkdir undo-tree
+# wget https://melpa.org/packages/undo-tree-20140509.522.el
+# cp undo-tree-*.el undo-tree/undo-tree.el
+# rm undo-tree-*.el
 
-mkdir themes
-wget https://melpa.org/packages/cyberpunk-theme-20160121.1712.el
-cp cyberpunk-theme-*.el themes/cyberpunk-theme.el
-rm cyberpunk-theme-*.el
+# mkdir themes
+# wget https://melpa.org/packages/cyberpunk-theme-20160121.1712.el
+# cp cyberpunk-theme-*.el themes/cyberpunk-theme.el
+# rm cyberpunk-theme-*.el
 
-wget https://melpa.org/packages/gruvbox-theme-20151227.113.el
-cp gruvbox-theme-*.el themes/gruvbox-theme.el
-rm gruvbox-theme-*.el
+# wget https://melpa.org/packages/gruvbox-theme-20151227.113.el
+# cp gruvbox-theme-*.el themes/gruvbox-theme.el
+# rm gruvbox-theme-*.el
 
-wget https://melpa.org/packages/solarized-theme-20160408.1143.tar
-tar -xf solarized*.tar
-mv solarized-*/* themes/
+# wget https://melpa.org/packages/solarized-theme-20160408.1143.tar
+# tar -xf solarized*.tar
+# mv solarized-*/* themes/
 
-mv *.tar $HOME/Downloads
-rmdir solarized-*
+# mv *.tar $HOME/Downloads
+# rmdir solarized-*
 
 # remember to configure tlp and enable programs that are to autostart (go doc, emacs server and xflux)
 # also, you probably have to update emacs packages
-
